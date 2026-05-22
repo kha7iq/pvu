8157385563ab9b2a32e91706387a008187bd3eeb
Khaliq <a.khaliq@outlook.my>
2026-05-22 15:00:50 +0800

    fix(version): change variables to local

--- internal/version/version.go
+++ internal/version/version.go
@@ -6,22 +6,20 @@ import (
 )
 
 var (
-	Version   = "dev"
-	Commit    = "none"
-	BuildDate = "unknown"
+	version   = "dev"
+	commit    = "none"
+	buildDate = "unknown"
 )
 
 func Info() string {
-	return fmt.Sprintf("version=%s commit=%s buildDate=%s go=%s %s/%s",
-		Version,
-		Commit,
-		BuildDate,
+
+	return fmt.Sprintf(
+		"Version: %s\nCommit: %s\nBuilt:   %s\nRuntime: %s on %s/%s",
+		version,
+		commit,
+		buildDate,
 		runtime.Version(),
 		runtime.GOOS,
 		runtime.GOARCH,
 	)
-}
-
-func Short() string {
-	return Version
 }
